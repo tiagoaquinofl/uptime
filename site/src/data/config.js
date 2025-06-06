d144ee17814450cd2d0845ec39e4c09e91f793ab
Tiago Aquino Fernandes Lopes <37239892+tiagoaquinofl@users.noreply.github.com>
2025-06-05 21:39:36 -0300

    Create config.js

---
+++ site/src/data/config.js
@@ -0,0 +1,43 @@
+{
+  "name": "📡 Monitoramento da Casa",
+  "description": "Monitoramento em tempo real da disponibilidade dos serviços pessoais do Tiago.",
+  "url": "https://s.tiagoaquino.com", 
+  "logo": "https://avatars.githubusercontent.com/u/73812536?s=200&v=4",
+  "theme": {
+    "name": "ocean",
+    "darkBackground": "#0f172a",
+    "lightBackground": "#e2e8f0"
+  },
+  "footer": {
+    "text": "Feito com ❤️ por Tiago usando [Upptime](https://github.com/upptime/upptime)",
+    "links": [
+      {
+        "text": "GitHub",
+        "url": "https://github.com/tiagoaquinofl/uptime"
+      },
+      {
+        "text": "Site pessoal",
+        "url": "https://tiagoaquino.com"
+      }
+    ]
+  },
+  "status": {
+    "good": "🟩 Online",
+    "degraded": "🟨 Instável",
+    "down": "🟥 Offline"
+  },
+  "navigation": [
+    {
+      "title": "🔍 Status",
+      "url": "/"
+    },
+    {
+      "title": "📈 Histórico",
+      "url": "/history"
+    },
+    {
+      "title": "📊 Resumo",
+      "url": "/summary"
+    }
+  ]
+}
